03ba5dabe659a530138f609a7ca561b66bcde07b
AndrewCRMartin <andrew@bioinf.org.uk>
2021-11-16 17:51:35 +0000

    Updates to process.sh to be more careful with filenames

--- src/process.sh
+++ src/process.sh
@@ -4,15 +4,21 @@ numberabpdb=${HOME}/git/absplit/src/numberabpdb.pl
 combinefaa=${HOME}/git/absplit/src/combinefaa.pl
 getfooter=${HOME}/git/absplit/src/getHETAndFooterRecords.pl
 
+# Split the file into component Fvs and antigens
 $absplit $input
 
-for file in *.pdb
+stem=`basename $input .ent`
+stem=`basename $stem  .pdb`
+
+# Repair the PDB files adding missing residues and trimming the SEQRES
+for file in ${stem}_*.pdb
 do
     echo $file
     pdbrepair -t $file > `basename $file .pdb`.fix
 done
 
-for file in *.fix
+# Extract the sequence for L and H and combine into one FASTA sequence
+for file in ${stem}_*.fix
 do
     tmpfaa=`basename $file .fix`.tmp
     faa=`basename $file .fix`.faa
@@ -22,21 +28,30 @@ do
     $combinefaa -l=$id $tmpfaa > $faa
 done
 
-for file in *.fix
+# Extract any HETATM footer, number the antibodies and add back the footer.
+# Finally renumber the atoms to reconstruct the MASTER and CONECT records
+for file in ${stem}_*.fix
 do
     footer=`basename $file .fix`.foot
     $getfooter $file > $footer
-    $numberabpdb -k $file `basename $file .fix`.kab
-    cat $footer >> `basename $file .fix`.kab
-    $numberabpdb -c $file `basename $file .fix`.cho
-    cat $footer >> `basename $file .fix`.cho
-    $numberabpdb -m $file `basename $file .fix`.mar
-    cat $footer >> `basename $file .fix`.mar
+
+    $numberabpdb -k $file `basename $file .fix`.tmp
+    cat $footer >> `basename $file .fix`.tmp
+    pdbrenum -d `basename $file .fix`.tmp > `basename $file .fix`.kab
+    
+    $numberabpdb -c $file `basename $file .fix`.tmp
+    cat $footer >> `basename $file .fix`.tmp
+    pdbrenum -d `basename $file .fix`.tmp > `basename $file .fix`.cho
+    
+    $numberabpdb -m $file `basename $file .fix`.tmp
+    cat $footer >> `basename $file .fix`.tmp
+    pdbrenum -d `basename $file .fix`.tmp > `basename $file .fix`.mar
     rm $footer
 done
 
-rm *.fix
-rm *.pdb
-rm *.tmp
+# Cleanup
+rm ${stem}_*.fix
+rm ${stem}_*.pdb
+rm ${stem}_*.tmp
 
 # pdbrepair -t pdb7mfa_0.num | pdbdummystrip | pdb2pir -s 
