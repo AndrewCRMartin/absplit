e40c207f3a8e5a5f45e292e5b0e65bdf5a5f310a
AndrewCRMartin <andrew@bioinf.org.uk>
2024-07-05 19:12:34 +0100

    precessall now does the NR step as well

--- src/processall.sh
+++ src/processall.sh
@@ -1,5 +1,6 @@
 ablist=$1
 process=${HOME}/git/absplit/src/process.sh
+nr=${HOME}/git/absplit/src/nr.pl
 pdbdir=/serv/data/pdb
 
 for ab in `awk -F_ '{print $1}' $ablist | sort -u`
@@ -8,4 +9,6 @@ do
     echo $pdbfile
     $process $pdbfile
 done
+
+$nr `pwd` > AbClusters.txt
 
