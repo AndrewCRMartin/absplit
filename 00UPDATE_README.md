8512a37c0ab8d33c9f57b97a53183a7b02771dae
AndrewCRMartin <andrew@bioinf.org.uk>
2025-09-12 18:06:59 +0100

    Added simple update script

---
+++ 00UPDATE_README.md
@@ -0,0 +1,5 @@
+How to run and update on serv1
+==============================
+
+nohup ./update.sh &
+
---
+++ update.sh
@@ -0,0 +1,29 @@
+#sacslist=/serv/www/html_abybank/sacs/list/antibodies.txt
+sacslist=/tmp/test.lis
+absplitdir=${HOME}/git/absplit/
+builddirtop=/data/abdbbuild
+webdir=/serv/www/html_abybank/abdb/snapshots/
+
+datestamp=`date +%4Y%m%d`
+abdbdir=abdb_${datestamp}
+builddir="${builddirtop}/${abdbdir}"
+mkdir -p $builddir
+if [ ! -d $builddir ]; then
+   echo "Couldn't create build directory: $builddir"
+   exit
+fi
+
+cd $builddir
+pwd=`pwd`
+if [ "X$pwd" != "X$builddir" ]; then
+    echo "Unable to change to build directory"
+else
+    nice -10 $absplitdir/src/processall.sh $sacslist &> update-${datestamp}.log
+    cd $builddirtop
+    zip -r ${abdbdir}.zip $abdbdir &>/dev/null
+    mv ${abdbdir}.zip $webdir
+    rm -rf $abdbdir
+fi
+
+
+
